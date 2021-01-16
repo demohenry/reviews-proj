2b1fca18a44556f4bcfd6f10004bfe4d31ea950e
Democrito <demohenry6@gmail.com>
2021-01-16 17:56:58 -0300

    Projeto Review Pronto

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    name: 'Maria Eduarda',
+    title: 'Gerente Geral',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: 'Jefferson Aguiar',
+    title: 'Marketing Digital',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+    name: 'Pedro Campos',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'Susana Amazonas',
+    title: 'CEO',
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam bibendum purus eu purus fringilla, ac varius mi pretium. Cras ullamcorper nulla felis.',
+  },
+];
+
+export default people;
